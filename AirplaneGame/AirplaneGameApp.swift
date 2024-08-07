ec89cf994c14d1f62047355c7e267cfd8957abd0
pablokostenko <pshkostenko@gmail.com>
2024-08-07 13:49:43 +0200

    Initial Commit

---
+++ AirplaneGame/AirplaneGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirplaneGameApp.swift
+//  AirplaneGame
+//
+//  Created by Pavlo Kostenko on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirplaneGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
